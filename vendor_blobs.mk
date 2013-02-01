992bea0d77bc277b42416d7e40136f488cafcc39
Giovanni Santini <itachi.sama.amaterasu@gmail.com>
2013-02-01 11:22:48 +0100

    Extras repo created ;D

---
+++ vendor_blobs.mk
@@ -0,0 +1,11 @@
+## MoltenMotherBoard extra vendor repo ;)
+
+## In order:
+# APNs
+# Hosts file
+# Superuser app
+PRODUCT_COPY_FILES += \
+    vendor/mmb/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/mmb/etc/hosts:system/etc/hosts \
+    vendor/mmb/app/Superuser.apk:system/app/Superuser.apk
+
