19b5595688e90d0d8eeaf3f9cdcdc5c732344ce2
Giovanni Santini <itachi.sama.amaterasu@gmail.com>
2013-06-17 22:45:02 +0200

    Adding ad-hoc CM9 MMB vendor config

---
+++ vendor_blobs_cm.mk
@@ -0,0 +1,18 @@
+## MoltenMotherBoard
+#
+#	This vendors are created by MMB-Team.
+#	They're needed to add some missing (important) features,
+#	already used by other Custom Roms (as CyanogenMod, AOKP, etc).
+#
+
+
+## Just the stuff not in CM
+
+## Hosts
+PRODUCT_COPY_FILES += \
+    vendor/mmb/etc/hosts:system/etc/hosts
+
+## User init support
+PRODUCT_COPY_FILES += \
+    vendor/mmb/etc/00banner:system/etc/init.d/01mmb
+
