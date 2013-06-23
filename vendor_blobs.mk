f4be64d2848a5b764f366033984a297f601bfc3d
mackief <davidepucci21@gmail.com>
2013-06-23 20:34:41 +0200

    Re-organized repo

--- vendor_blobs.mk
+++ vendor_blobs.mk
@@ -4,42 +4,39 @@
 #	They're needed to add some missing (important) features,
 #	already used by other Custom Roms (as CyanogenMod, AOKP, etc).
 
-## APNs
+## APNs && /etc stuff
 PRODUCT_COPY_FILES += \
-    vendor/mmb/etc/apns-conf.xml:system/etc/apns-conf.xml \
-    vendor/mmb/etc/profile:system/etc/profile \
-    vendor/mmb/etc/resolv.conf:system/etc/resolv.conf \
-    vendor/mmb/etc/spn-conf.xml:system/etc/spn-conf.xml \
-    vendor/mmb/etc/sysctl.conf:system/etc/sysctl.conf
-
-## Hosts
-PRODUCT_COPY_FILES += \
-    vendor/mmb/etc/hosts:system/etc/hosts
+    vendor/mmb/proprietary/system/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/mmb/proprietary/system/etc/hosts:system/etc/hosts \
+    vendor/mmb/proprietary/system/etc/profile:system/etc/profile \
+    vendor/mmb/proprietary/system/etc/resolv.conf:system/etc/resolv.conf \
+    vendor/mmb/proprietary/system/etc/spn-conf.xml:system/etc/spn-conf.xml \
+    vendor/mmb/proprietary/system/etc/sysctl.conf:system/etc/sysctl.conf
 
 ## Init.d support Enabler
 PRODUCT_COPY_FILES += \
-    vendor/mmb/bin/sysinit:system/bin/sysinit \
-    vendor/mmb/ramdisk/init.local.rc:root/init.local.rc
+    vendor/mmb/proprietary/system/bin/sysinit:system/bin/sysinit \
+    vendor/mmb/proprietary/ramdisk/init.local.rc:root/init.local.rc
 
 ## User init support
 PRODUCT_COPY_FILES += \
-    vendor/mmb/etc/00banner:system/etc/init.d/00banner \
-    vendor/mmb/etc/90userinit:system/etc/init.d/90userinit
+    vendor/mmb/proprietary/system/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/mmb/proprietary/system/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 ## Camera effects blobs
 PRODUCT_COPY_FILES += \
-    vendor/mmb/camera/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/mmb/camera/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/mmb/proprietary/system/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/mmb/proprietary/system/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 ## XBIN files
 PRODUCT_COPY_FILES += \
-    vendor/mmb/xbin/bash:system/xbin/bash \
-    vendor/mmb/xbin/dropbear-keygen:system/xbin/dropbear-keygen \
-    vendor/mmb/xbin/htop:system/xbin/htop \
-    vendor/mmb/xbin/irssi:system/xbin/irssi \
-    vendor/mmb/xbin/nano:system/xbin/nano \
-    vendor/mmb/xbin/openvpn-up.sh:system/xbin/openvpn-up.sh \
-    vendor/mmb/xbin/powertop:system/xbin/powertop
+    vendor/mmb/proprietary/system/xbin/bash:system/xbin/bash \
+    vendor/mmb/proprietary/system/xbin/dropbear-keygen:system/xbin/dropbear-keygen \
+    vendor/mmb/proprietary/system/xbin/htop:system/xbin/htop \
+    vendor/mmb/proprietary/system/xbin/irssi:system/xbin/irssi \
+    vendor/mmb/proprietary/system/xbin/nano:system/xbin/nano \
+    vendor/mmb/proprietary/system/xbin/openvpn-up.sh:system/xbin/openvpn-up.sh \
+    vendor/mmb/proprietary/system/xbin/powertop:system/xbin/powertop
 
 ## Other Packages
 PRODUCT_PACKAGES := \
