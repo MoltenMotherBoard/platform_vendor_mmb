df06aad05f71dd77dacf756f591643afc9d5aaa1
Giovanni Santini <giovannisantini93@yahoo.it>
2013-12-10 21:46:26 +0100

    Workaround for mobile DNS
    Change-Id: I2f2a3500c5aa507d1875bb64db0214ee2a5a0755

--- vendor_blobs.mk
+++ vendor_blobs.mk
@@ -21,6 +21,7 @@ PRODUCT_COPY_FILES += \
 ## User init support
 PRODUCT_COPY_FILES += \
     vendor/mmb/proprietary/system/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/mmb/proprietary/system/etc/init.d/50dns:system/etc/init.d/50dns \
     vendor/mmb/proprietary/system/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 ## Camera effects blobs
